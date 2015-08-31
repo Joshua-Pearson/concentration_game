2177175936433db95195e7124d5252801e35c347
Joshua-Pearson <j_d_pearson@yahoo.com>
2015-08-31 04:32:32 -0700

    adding correct routes

--- concentration_game/config/routes.rb
+++ concentration_game/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :games
+  resources :games, only: [:create, :show]
+  post "games/turn"
+  resources :players, only: [:create]
   root "games#welcome"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
