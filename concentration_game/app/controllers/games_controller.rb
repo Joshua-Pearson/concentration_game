6dfd6f1dbea111d4825f5d4589cf36cdb85c6896
Joshua-Pearson <j_d_pearson@yahoo.com>
2015-09-01 01:04:55 -0700

    final commit

--- concentration_game/app/controllers/games_controller.rb
+++ concentration_game/app/controllers/games_controller.rb
@@ -19,6 +19,8 @@ class GamesController < ApplicationController
   end
 
   def turn
+    @game.update_attributes(turn: @game.turn += 1)
+
     if @card_one.is_matching?(@card_two)
       @card_one.update_attributes(matched: true) && @card_two.update_attributes(matched: true)
       @player.update_attributes(points: @player.points += 1)
@@ -40,14 +42,13 @@ private
   end
 
   def start_new_game
-    @game.deck.set_cards_as_unmatched
+    @game.reset
     @cards_left = @game.deck.shuffle
     @cards_on_board = @cards_left.shift(22)
   end
 
   def set_variables_for_each_turn
-    @cards_left ||= Card.where(id: params[:cards_left].split(" ")).shuffle
-    @game.update_attributes(turn: @game.turn += 1)
+    @cards_left = Card.where(id: params[:cards_left].split(" ")).shuffle
     @card_one = Card.find_by(id: params[:card_one_id])
     @card_two = Card.find_by(id: params[:card_two_id])
   end
--- concentration_game/app/models/game.rb
+++ concentration_game/app/models/game.rb
@@ -4,4 +4,10 @@ class Game < ActiveRecord::Base
   has_one :player
   has_many :cards, through: :deck
 
+  def reset
+    update_attributes(turn: 0)
+    player.update_attributes(points: 0)
+    deck.set_cards_as_unmatched
+  end
+
 end
